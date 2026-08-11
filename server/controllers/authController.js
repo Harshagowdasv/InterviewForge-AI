72dc8010aa202e4925611e10838f8a6825776486
Harsha Gowda SV <harshagowdasv02@gmail.com>
2026-08-11 06:31:54 +0530

    feat : Added Some Basic Code

---
+++ server/controllers/authController.js
@@ -0,0 +1,11 @@
+exports.getAuth = (req,res)=>{
+    res.send("Auth Route Working"); 
+}
+
+exports.register = (req,res)=>{
+    res.send("Register Route Working");
+}
+
+exports.login = (req,res)=>{
+    res.send("Login Route Working");
+}
---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+
+app.use("/api/users", userRoutes);
+
+app.use("/api/auth", authRoutes);
+
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+    res.send("InterviewForge AI Backend Running...");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
