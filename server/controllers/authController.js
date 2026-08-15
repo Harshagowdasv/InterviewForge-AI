cf49e4fb6f1c42050617d836deff9b30551f9730
Harsha Gowda SV <harshagowdasv02@gmail.com>
2026-08-15 23:26:32 +0530

    feat : included JWT Token Verification Logic

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -95,3 +95,29 @@ exports.login = async (req,res)=>{
         });
     }
 }
+
+exports.getProfile = async(req,res)=>{
+    try{
+
+        const user = await User.findById(req.user.id);
+
+        if(!user){
+            return res.status(404).json({
+                success: false,
+                message:"User not found"
+            });
+        }
+
+        return res.status(200).json({
+            success:true,
+            user
+        })
+
+    }catch(error){
+        return res.status(500).json({
+            success:false,
+            message: error.message
+        });
+
+    };
+}
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,8 +4,12 @@ const router = express.Router();
 
 const authController = require("../controllers/authController");
 
+const { verifyToken } = require("../middleware/authMiddleware");
+const { getProfile } = require("../controllers/authController");
+
 router.get("/", authController.getAuth);
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+router.get("/profile",verifyToken,getProfile);
 
 module.exports = router;
